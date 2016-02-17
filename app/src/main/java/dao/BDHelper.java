e388589dd1ccd028806093b423568abeb0192fc2
Pedro_Abad <pedroabad@upeu.edu.pe>
2016-02-17 15:31:14 -0500

    C2: Creacion de Mensajes, bdhelper y 2do Activity

---
+++ app/src/main/java/dao/BDHelper.java
@@ -0,0 +1,9 @@
+package dao;
+
+/**
+ * Created by Programacion on 17/02/2016.
+ */
+public class BDHelper {
+
+
+}
---
+++ app/src/main/java/util/Mensajes.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by Programacion on 17/02/2016.
+ */
+public class Mensajes {
+}
